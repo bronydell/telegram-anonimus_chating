1703fbc53f4c7b707af829729cb3f0d417aa9522
Bronydell <littleponyapps@gmail.com>
2017-03-16 08:00:45 +0300

    DEBUG -> INFO

--- start.py
+++ start.py
@@ -11,7 +11,7 @@ from actions import super_actions
 from database import admin_db
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                  level=logging.DEBUG)
+                  level=logging.INFO)
 
 
 def error_callback(bot, update, error):
